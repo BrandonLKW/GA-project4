88dedd2e6e758dc32ace55b500cd51b6960d7e70
BrandonLKW <lum371@hotmail.com>
2024-02-08 20:43:02 +0800

    added simple nav for pages

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,6 +2,10 @@ import { Button } from "@mui/material";
 // import { useState } from 'react'
 import './App.css'
 import NavBar from "../../components/NavBar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "../MainPage/MainPage";
+import PlanPage from "../PlanPage/PlanPage";
+import MetricsPage from "../MetricsPage/MetricsPage";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,9 +13,14 @@ function App() {
   return (
     <div className="appBody">
       <NavBar />
-        <h1>LOGO HERE</h1>
-        <Button>Login</Button>
-        <Button>Signup</Button>
+      <h1>LOGO HERE</h1>
+      <Button>Login</Button>
+      <Button>Signup</Button>
+      <Routes>
+        <Route path="/main" element={<MainPage />}/>
+        <Route path="/plan" element={<PlanPage />}/>
+        <Route path="/metrics" element={<MetricsPage />}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/MetricsPage/MetricsPage.tsx
@@ -0,0 +1,7 @@
+export default function MetricsPage(){
+    return(
+        <div>
+            <h1>Metrics Page</h1>
+        </div>
+    );
+}
