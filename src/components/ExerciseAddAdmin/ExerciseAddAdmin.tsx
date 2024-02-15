fe640b56a89487dcf173df8373276fce1c53be3d
BrandonLKW <lum371@hotmail.com>
2024-02-16 00:51:44 +0800

    added styling for amind exercise pages

--- src/components/ExerciseAddAdmin/ExerciseAddAdmin.tsx
+++ src/components/ExerciseAddAdmin/ExerciseAddAdmin.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Button, FormControl, TextField } from "@mui/material";
+import { Button, FormControl, TextField, Typography } from "@mui/material";
 import { Exercise } from "../../../models/Exercise";
+import "./ExerciseAddAdmin.css";
 
 type ExerciseAddAdminProps = {
     selectedExercise: Exercise;
@@ -37,19 +38,17 @@ export default function ExerciseAddAdmin({ selectedExercise, manageExercise } : 
     }
 
     return (
-        <>
-            <h1 className="">{action} Exercise</h1>
-            <div className="">
-                <FormControl variant="standard">
-                    <TextField label="Name" name="name" variant="outlined" value={workingExercise.name} onChange={handleTextChange}/>
-                    <TextField label="Description" name="description" variant="outlined" value={workingExercise.description} onChange={handleTextChange}/>
-                    <TextField label="Muscle Group" name="muscle_group" variant="outlined" value={workingExercise.muscle_group} onChange={handleTextChange}/>
-                </FormControl>
+        <div className="exerciseAddAdmin">
+            <Typography variant="h4">{action} Exercise</Typography>
+            <FormControl className="exerciseAddAdminBody" variant="standard">
+                <TextField label="Name" name="name" variant="outlined" value={workingExercise.name} onChange={handleTextChange}/>
+                <TextField label="Description" name="description" variant="outlined" value={workingExercise.description} onChange={handleTextChange}/>
+                <TextField label="Muscle Group" name="muscle_group" variant="outlined" value={workingExercise.muscle_group} onChange={handleTextChange}/>
+            </FormControl>
+            <div className="exerciseAddAdminFooter">
+                <Button variant="contained" onClick={handleResetButtonClick}>Clear</Button>
+                <Button variant="contained" onClick={handleAddButtonClick}>{action.toUpperCase()}</Button>
             </div>
-            <div className="">
-                <Button onClick={handleResetButtonClick}>Clear</Button>
-                <Button onClick={handleAddButtonClick}>{action.toUpperCase()}</Button>
-            </div>
-        </>
+        </div>
     );
 }
